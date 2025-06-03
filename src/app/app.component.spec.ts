958bbcd5dfb1f3df27882ac2270ba748ea1676bb
Ed Fallin <edfallin@earthlink.net>
2025-06-03 15:48:16 -0700

    Fixed some tests to make them pass with new app title. Added necessary components for actual app contents.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe("AppComponent", () => {
   it(`should have the "ToDoList" title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("ToDoList");
+    expect(app.title).toEqual("To-Do List");
   });
 
   it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain("Hello, ToDoList");
+    expect(compiled.querySelector("h1")?.textContent).toContain("Hello, To-Do List!");
   });
 });
