c40633a9318b8d6c1e6c9f2afdda9c28d0abf4c0
Ed Fallin <edfallin@earthlink.net>
2025-06-04 17:10:49 -0700

    Implemented most or all of endpoints, with fake local data for now.

--- ToDoListServer/ToDoListServer/Controllers/ItemController.cs
+++ ToDoListServer/ToDoListServer/Controllers/ItemController.cs
@@ -3,25 +3,53 @@ using Microsoft.AspNetCore.Mvc;
 namespace ToDoListServer;
 
 [ApiController]
-[Route("[controller]")]
+[Route("items")]
 public class ItemController : ControllerBase {
-  private List<Item> fakes = new() {
-    new Item(1, 1, "Task A1", false),
-    new Item(2, 2, "Task B2", false),
-    new Item(3, 3, "Task C3", false),
-    new Item(4, 4, "Task D4", false),
-    new Item(5, 5, "Task E5", false)
-  };
+    private List<Item> fakes = new() {
+        new Item(1, 1, "Task A1", false),
+        new Item(2, 2, "Task B2", false),
+        new Item(3, 3, "Task C3", false),
+        new Item(4, 4, "Task D4", false),
+        new Item(5, 5, "Task E5", false)
+    };
 
-  private readonly ILogger<ItemController> _logger;
+    private readonly ILogger<ItemController> _logger;
 
-  public ItemController(ILogger<ItemController> logger) {
-    _logger = logger;
-  }
+    public ItemController(ILogger<ItemController> logger) {
+        _logger = logger;
+    }
 
-  [HttpGet("")]
-  public async Task<ActionResult<IEnumerable<Item>>> GetAsync() {
-      List<Item> items = await Task.FromResult(fakes);
-      return Ok(items);
-  }
+    [HttpGet("")]
+    public async Task<ActionResult<IEnumerable<Item>>> GetAllItemsAsync() {
+        List<Item> items = await Task.FromResult(fakes);
+        return Ok(items);
+    }
+
+    [HttpPost("")]
+    public async Task<ActionResult> PostNewItemAsync(NewItem added) {
+        int next = fakes.Count + 1;
+        Item item = new(next, next, added.Task, false);
+        fakes.Add(item);
+
+        // Request URL properties all include trailing "/". 
+        string url = $"{Request.Scheme}://{Request.Host}{Request.Path}{item.UID}";
+
+        return Created(url, item);
+    }
+    
+    [HttpPut("")]
+    public async Task<ActionResult> PutChangedItemAsync(Item changed) {
+        int target = fakes.FindIndex(x => x.UID == changed.UID);
+        fakes[target] = changed;
+        Console.WriteLine(fakes);
+        return NoContent();
+    }
+    
+    [HttpDelete("{uid}")]
+    public async Task<ActionResult> DeleteItemAsync(int uid) {
+        int target = fakes.FindIndex(x => x.UID == uid);
+        fakes.RemoveAt(target);
+        Console.WriteLine(fakes);
+        return NoContent();
+    }
 }
---
+++ ToDoListServer/ToDoListServer/Definitions/NewItem.cs
@@ -0,0 +1,5 @@
+namespace ToDoListServer;
+
+public class NewItem {
+    public string Task { get; set; }
+}
